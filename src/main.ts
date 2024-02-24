c6d37adec918a1ffb31e66780d8a070ad28ea543
eunmin-kim <53runtime@gmail.com>
2024-02-24 15:43:53 +0900

    Feature: main.ts에서 ValidationPipe를 추가하고 전역적으로 사용하도록 구현
             이 커밋은 main.ts의 import 부분에 '@nestjs/common'에서 ValidationPipe를 추가하는 것을 포함하고 있습니다. 추가적으로, ValidationPipe는 bootstrap() 함수에서 전역적으로 활용되며, 이를 통해 전체 애플리케이션에 걸쳐 데이터의 정확성과 무결성을 확인하는 검증을 처리합니다.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerConfig } from '../config/swagger.config';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,14 @@ async function bootstrap() {
    * Swagger 생성
    */
   const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(port);
 }
 
